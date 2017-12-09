fb6ee0fb37bc29303af5d054402a5d8554539ef4
Sandy Armstrong <sandy@xamarin.com>
2017-12-08 16:01:10 -0800

    SignatureHelp: Prevent showing static methods in instance context and vice versa
    Fixes https://github.com/Microsoft/workbooks/issues/64.

---
+++ Clients/Xamarin.Interactive.Client/RoslynInternals/InternalExtensions.cs
@@ -0,0 +1,42 @@
+﻿//
+// Author:
+//   Sandy Armstrong <sandy@xamarin.com>
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Reflection;
+
+using Microsoft.CodeAnalysis;
+
+namespace Xamarin.Interactive.RoslynInternals
+{
+    static class InternalExtensions
+    {
+        static readonly Type symbolExtensionsType = typeof (Workspace).Assembly.GetType (
+            "Microsoft.CodeAnalysis.Shared.Extensions.ISymbolExtensions");
+
+        static readonly MethodInfo isAccessibleWithin = symbolExtensionsType.GetMethod (
+            "IsAccessibleWithin",
+            new [] {
+                typeof (ISymbol),
+                typeof (ISymbol),
+                typeof (ITypeSymbol)
+            });
+        static readonly MethodInfo getSymbolType = symbolExtensionsType.GetMethod (
+            "GetSymbolType",
+            new [] { typeof (ISymbol) });
+
+        public static bool IsAccessibleWithin (
+            this ISymbol symbol,
+            ISymbol within,
+            ITypeSymbol throughTypeOpt = null)
+            => (bool)isAccessibleWithin.Invoke (
+                null,
+                new object [] { symbol, within, throughTypeOpt });
+
+        public static ITypeSymbol GetSymbolType (this ISymbol symbol)
+            => (ITypeSymbol)getSymbolType.Invoke (null, new object [] { symbol });
+    }
+}
