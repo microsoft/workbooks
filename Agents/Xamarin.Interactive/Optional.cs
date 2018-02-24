54c7120bd0a905c5784c2e50817ff9650c244b3b
Aaron Bockover <abock@microsoft.com>
2018-02-23 20:34:07 -0500

    Optional<T>: add

---
+++ Agents/Xamarin.Interactive/Optional.cs
@@ -0,0 +1,32 @@
+﻿//
+// Author:
+//   Aaron Bockover <abock@microsoft.com>
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Xamarin.Interactive
+{
+    public struct Optional<T>
+    {
+        public bool HasValue { get; }
+        public T Value { get; }
+
+        public Optional (T value)
+        {
+            HasValue = true;
+            Value = value;
+        }
+
+        public override string ToString ()
+            => HasValue
+                ? Value?.ToString () ?? "(null)"
+                : "(unspecified)";
+
+        public T GetValueOrDefault (T @default = default (T))
+            => HasValue ? Value : @default;
+
+        public static implicit operator Optional<T> (T value)
+            => new Optional<T> (value);
+    }
+}
