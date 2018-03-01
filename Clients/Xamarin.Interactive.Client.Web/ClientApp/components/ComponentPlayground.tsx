59d5d01c007fb98fbdf790510f716bb9334fc24b
Aaron Bockover <abock@microsoft.com>
2018-03-01 17:05:43 -0500

    Playground: add AbortEvaluationButton

--- Clients/Xamarin.Interactive.Client.Web/ClientApp/components/ComponentPlayground.tsx
+++ Clients/Xamarin.Interactive.Client.Web/ClientApp/components/ComponentPlayground.tsx
@@ -7,6 +7,15 @@ export class ComponentPlayground extends React.Component<RouteComponentProps<{}>
         return (
             <article>
                 <h1>Component Playground for Design</h1>
+                <h2>Abort Evaluation Button</h2>
+                <p>
+                    This button acts as a status indicator that a code cell is evaluating; when hovering
+                    or otherwise interacting with it however, it will indicate that it may be used to
+                    abort the evaluation.
+                </p>
+                <p>
+                    <AbortEvaluationButton />
+                </p>
             </article>
         )
     }
