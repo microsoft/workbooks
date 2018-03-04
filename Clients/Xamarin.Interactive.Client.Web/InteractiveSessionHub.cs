359c140d5d436a9d0a3c74bf65b41272f563e0bb
Aaron Bockover <abock@microsoft.com>
2018-03-04 12:33:47 -0500

    Web: fix session binding regression

--- Clients/Xamarin.Interactive.Client.Web/InteractiveSessionHub.cs
+++ Clients/Xamarin.Interactive.Client.Web/InteractiveSessionHub.cs
@@ -71,14 +71,14 @@ namespace Xamarin.Interactive.Client.Web
 
         async Task InitializeClientSessionAsync (string connectionId, ClientSession session)
         {
-            serviceProvider
-                .GetInteractiveSessionHubManager ()
-                .BindClientSession (connectionId, session);
-
             session.InitializeViewControllers (new WebClientSessionViewControllers (connectionId, serviceProvider));
 
             await session.InitializeAsync ();
             await session.EnsureAgentConnectionAsync ();
+
+            serviceProvider
+                .GetInteractiveSessionHubManager ()
+                .BindClientSession (connectionId, session);
         }
 
         public async Task<string> InsertCodeCell (
