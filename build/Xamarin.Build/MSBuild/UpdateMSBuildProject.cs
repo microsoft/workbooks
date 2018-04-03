56de3023ed16d666b92143ccd67c6a688459dd21
Aaron Bockover <abock@xamarin.com>
2018-04-03 11:20:58 -0400

    Build: simplify metadata targets, persist properties
    This eliminates Metadata.targets and reduces it down to a single
    target now in CoreBuild.proj. All CommonGitInfo, BuildEnvironment,
    and ReleaseVersion properties are computed via the Configure target
    and persisted to global configuration.
    
    No targets except Configure need to depend on anything.

---
+++ build/Xamarin.Build/MSBuild/UpdateMSBuildProject.cs
@@ -0,0 +1,39 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Text;
+
+using Microsoft.Build.Construction;
+using Microsoft.Build.Evaluation;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Xamarin.MSBuild
+{
+    public sealed class UpdateMSBuildProject : Task
+    {
+        public string Project { get; set; }
+
+        public bool AsNewProject { get; set; }
+
+        public ITaskItem [] Properties { get; set; }
+
+        public override bool Execute ()
+        {
+            var projectCollection = new ProjectCollection ();
+            var project = AsNewProject
+                ? new Project (projectCollection, NewProjectFileOptions.None) { FullPath = Project }
+                : projectCollection.LoadProject (Project);
+
+            if (Properties != null) {
+                foreach (var property in Properties)
+                    project.SetProperty (property.ItemSpec, property.GetMetadata ("Value"));
+            }
+
+            project.Save (Encoding.UTF8);
+
+            return true;
+        }
+    }
+}
